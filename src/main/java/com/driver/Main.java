b5d43042f1754543757b27c77c2e3d33a1d5c8d4
Sanket Garud <sanketgarud10@gmail.com>
2023-04-12 17:35:50 +0530

    assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]){
+        RWOnly obj = new RWOnly();
+        obj.setName("Sanket");
+
+        String value = obj.getName();
+        System.out.println(value);
+    }
+
+
 }
